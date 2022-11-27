819502cd2db990c90b0d28390593e0ae6953b415
Diego <diegoulloao@icloud.com>
2022-11-27 19:47:59 -0300

    feat: storybook

---
+++ .storybook/main.cjs
@@ -0,0 +1,20 @@
+const { typescript: preprocessTs } = require('svelte-preprocess');
+
+module.exports = {
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions'
+	],
+	framework: '@storybook/svelte',
+	core: {
+		builder: '@storybook/builder-vite'
+	},
+	svelteOptions: {
+		preprocess: [preprocessTs()]
+	},
+	features: {
+		storyStoreV7: true
+	}
+};
