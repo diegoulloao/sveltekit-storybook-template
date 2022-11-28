d7d443a90c52cb4281bf1a08b7ef87e8fff20987
Carlos Knopel <carlos.knopel@gmail.com>
2022-11-27 22:02:33 -0300

    feat: add husky, commitlint and lint-staged

--- src/lib/components/Button/Button.stories.svelte
+++ src/lib/components/Button/Button.stories.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
-  import Button from './Button.svelte';
-  // import { Button } from "$lib"
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+	// import { Button } from "$lib"
 
 	const argTypes = {
 		backgroundColor: { control: 'color' },
@@ -12,21 +12,21 @@
 			control: { type: 'select' },
 			options: ['small', 'medium', 'large']
 		}
-	}
+	};
 
-  const onClick = () => {
-    console.log("clicked")
-  }
+	const onClick = () => {
+		console.log('clicked');
+	};
 </script>
 
 <Meta title="Example/Button" component={Button} {argTypes} />
 
 <Template let:args>
-  <Button {...args} on:click={onClick} />
+	<Button {...args} on:click={onClick} />
 </Template>
 
-<Story name="Primary" args={{ label: "Button", primary: true }}/>
-<Story name="Tailwind" args={{ label: "Button using tailwind bg-red-400", tailwind: true }}/>
-<Story name="Secondary" args={{ label: "Button" }} source />
-<Story name="Large" args={{ label: "Button", size: "large" }} />
-<Story name="Small" args={{ label: "Button", size: "small" }} />
+<Story name="Primary" args={{ label: 'Button', primary: true }} />
+<Story name="Tailwind" args={{ label: 'Button using tailwind bg-red-400', tailwind: true }} />
+<Story name="Secondary" args={{ label: 'Button' }} source />
+<Story name="Large" args={{ label: 'Button', size: 'large' }} />
+<Story name="Small" args={{ label: 'Button', size: 'small' }} />
--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,48 +1,49 @@
 <script lang="ts">
-  import './button.css';
-  import { createEventDispatcher } from 'svelte';
+	import './button.css';
+	import { createEventDispatcher } from 'svelte';
 
-  export let tailwind: boolean = false;
+	export let tailwind: boolean = false;
 
-  /**
-   * Is this the principal call to action on the page?
-   */
-  export let primary: boolean = false;
+	/**
+	 * Is this the principal call to action on the page?
+	 */
+	export let primary: boolean = false;
 
-  /**
-   * What background color to use
-   */
-  export let backgroundColor;
-  /**
-   * How large should the button be?
-   */
-  export let size = 'medium';
-  /**
-   * Button contents
-   */
-  export let label = '';
+	/**
+	 * What background color to use
+	 */
+	export let backgroundColor;
+	/**
+	 * How large should the button be?
+	 */
+	export let size = 'medium';
+	/**
+	 * Button contents
+	 */
+	export let label = '';
 
-  let mode = () => {
-    if (tailwind) return 'bg-red-400 text-white';
-    return primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  }
+	let mode = () => {
+		if (tailwind) return 'bg-red-400 text-white';
+		return primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+	};
 
-  let style = backgroundColor ? `background-color: ${backgroundColor}` : '';
+	let style = backgroundColor ? `background-color: ${backgroundColor}` : '';
 
-  const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-  /**
-   * Optional click handler
-   */
-  function onClick(event) {
-    dispatch('click', event);
-  }
+	/**
+	 * Optional click handler
+	 */
+	function onClick(event: Event) {
+		dispatch('click', event);
+	}
 </script>
 
 <button
-  type="button"
-  class={['storybook-button', `storybook-button--${size}`, mode()].join(' ')}
-  {style}
-  on:click={onClick}>
-  {label}
+	type="button"
+	class={['storybook-button', `storybook-button--${size}`, mode()].join(' ')}
+	{style}
+	on:click={onClick}
+>
+	{label}
 </button>
